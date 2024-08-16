c6a2b0857d5ef4e2ab7936a1da39bb5089bcb4ed
Leonid Zelov <112327440+LeoniD994@users.noreply.github.com>
2024-08-16 21:02:03 +0300

    fix navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,21 @@
 import { NavLink } from "react-router-dom";
-import style from "./Navigation.module.css";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import css from "./Navigation.module.css";
 
 const Navigation = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
-    <nav className={style.navigation}>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/contacts">Contacts</NavLink>
+    <nav>
+      <NavLink className={css.link} to="/">
+        Home
+      </NavLink>
+      {isLoggedIn && (
+        <NavLink className={css.link} to="/contacts">
+          Contacts
+        </NavLink>
+      )}
     </nav>
   );
 };
-
 export default Navigation;
